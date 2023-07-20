752a9c033c96573d2f2a5ecf5d8b967f58888348
unknown <cuch.aleksander@gmail.com>
2023-07-20 15:34:44 +0200

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
-import './globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+import { MainNavigation } from '@/components/layout/main-navigation'
+
+import './globals.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MainNavigation />
+        {children}
+      </body>
     </html>
   )
 }
