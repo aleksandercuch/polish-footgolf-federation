ef1fb8a18c9d64cf299285bc59ac63715c85dd83
unknown <cuch.aleksander@gmail.com>
2023-07-25 14:22:33 +0200

    management desktop version

--- src/components/layout/main-navigation.tsx
+++ src/components/layout/main-navigation.tsx
@@ -24,102 +24,106 @@ export const MainNavigation = () => {
 
                 <nav className={classes.navigation__mobile__nav}>
                     <ul className={classes.navigation__mobile__list}>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("navHistoryBarFirst")}</Link></li>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("navHistoryBarSecond")}</Link></li>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("management")}</Link></li>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("statue")}</Link></li>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("postolowoLeauge")}</Link></li>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("polishOpen")}</Link></li>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("fifaWorldCup")}</Link></li>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("playerProfiles")}</Link></li>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("recordsFifa")}</Link></li>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("regulations")}</Link></li>
-                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("login")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.homePage")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.historyBarFirst")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.historyBarSecond")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="/management" className={classes.navigation__mobile__link}>{t("NAV.management")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.statue")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.postolowoLeauge")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.polishOpen")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.fifaWorldCup")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.playerProfiles")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.recordsFifa")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.regulations")}</Link></li>
+                        <li className={classes.navigation__mobile__item}><Link href="#" className={classes.navigation__mobile__link}>{t("NAV.login")}</Link></li>
 
                         </ul>
                 </nav>
             </div>
             <div className={classes.navigation__menu}>
                 <div className={classes.navigation__menu__item}>
-                    <a>{t("federation")}</a>
+                    <Link href="/">{t("NAV.homePage")}</Link>
+                </div>
+                <div className={classes.navigation__menu__item}>
+                    <a>{t("NAV.federation")}</a>
                     <div className={classes.navigation__menu__dropdown}>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <a>{t("history")}</a>
+                            <a>{t("NAV.history")}</a>
                             <div className={classes.navigation__menu__horizontalDropdown}>
                                 <div className={classes.navigation__menu__horizontalDropdown__item}>
-                                    <Link href="#">{t('navHistoryBarFirst')}</Link>
+                                    <Link href="#">{t('NAV.historyBarFirst')}</Link>
                                 </div>
                                 <div className={classes.navigation__menu__horizontalDropdown__item}>
-                                    <Link href="#">{t('navHistoryBarSecond')}</Link>
+                                    <Link href="#">{t('NAV.historyBarSecond')}</Link>
                                 </div>
                                 <div className={classes.navigation__menu__horizontalDropdown__item}>
-                                    <Link href="#">{t('navHistoryBarThird')}</Link>
+                                    <Link href="#">{t('NAV.historyBarThird')}</Link>
                                 </div>
                                 <div className={classes.navigation__menu__horizontalDropdown__item}>
-                                    <Link href="#">{t('navHistoryBarFourth')}</Link>
+                                    <Link href="#">{t('NAV.historyBarFourth')}</Link>
                                 </div>
                             </div>
                         </div>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <Link href="#">{t('management')}</Link>
+                            <Link href="/management">{t('NAV.management')}</Link>
                         </div>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <Link href="#">{t("statue")}</Link>
+                            <Link href="#">{t("NAV.statue")}</Link>
                         </div>
                     </div>
                 </div>
                 <div className={classes.navigation__menu__item}>
-                    <a>{t("games")}</a>
+                    <a>{t("NAV.games")}</a>
                     <div className={classes.navigation__menu__dropdown}>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <a>{t("postolowoLeauge")}</a>
+                            <a>{t("NAV.postolowoLeauge")}</a>
                             <div className={classes.navigation__menu__horizontalDropdown}>
                                 <div className={classes.navigation__menu__horizontalDropdown__item}>
-                                    <Link href="#">{t("leaugeItemOne")}</Link>
+                                    <Link href="#">{t("NAV.leaugeItemOne")}</Link>
                                 </div>
                                 <div className={classes.navigation__menu__horizontalDropdown__item}>
-                                    <Link href="#">{t("leaugeItemTwo")}</Link>
+                                    <Link href="#">{t("NAV.leaugeItemTwo")}</Link>
                                 </div>
                                 <div className={classes.navigation__menu__horizontalDropdown__item}>
-                                    <Link href="#">{t("leaugeItemThree")}</Link>
+                                    <Link href="#">{t("NAV.leaugeItemThree")}</Link>
                                 </div>
                             </div>
                         </div>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <Link href="#">{t("polishOpen")}</Link>
+                            <Link href="#">{t("NAV.polishOpen")}</Link>
                         </div>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <Link href="#">{t("fifaWorldCup")}</Link>
+                            <Link href="#">{t("NAV.fifaWorldCup")}</Link>
                         </div>
                     </div>
                 </div>
                 <div className={classes.navigation__menu__item}>
-                    <a>{t("cadre")}</a>
+                    <a>{t("NAV.cadre")}</a>
                     <div className={classes.navigation__menu__dropdown}>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <Link href="#">{t("playerProfiles")}</Link>
+                            <Link href="#">{t("NAV.playerProfiles")}</Link>
                         </div>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <Link href="#">{t("list")}</Link>
+                            <Link href="#">{t("NAV.list")}</Link>
                         </div>
                     </div>
                 </div>
                 <div className={classes.navigation__menu__item}>
-                    <a>{t("recordsAndRegulations")}</a>
+                    <a>{t("NAV.recordsAndRegulations")}</a>
                     <div className={classes.navigation__menu__dropdown}>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <Link href="#">{t("recordsFifa")}</Link>
+                            <Link href="#">{t("NAV.recordsFifa")}</Link>
                         </div>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <Link href="#">{t("regulationsLeauge")}</Link>
+                            <Link href="#">{t("NAV.regulationsLeauge")}</Link>
                         </div>
                         <div className={classes.navigation__menu__dropdown__item}>
-                            <Link href="#">{t("regulationsPolishOpen")}</Link>
+                            <Link href="#">{t("NAV.regulationsPolishOpen")}</Link>
                         </div>
                     </div>
                 </div>
                 <div className={classes.navigation__menu__item}>
-                    <Link href="#">{t("login")}</Link>
+                    <Link href="#">{t("NAV.login")}</Link>
                 </div>
             </div>
         </div>
