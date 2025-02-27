e6d06608ee2e4a4ef37d207368234d458c55a343
unknown <cuch.aleksander@gmail.com>
2025-02-27 17:56:53 +0100

    PFFG-19 fix news and regulations UI

--- src/components/mainpage/News.tsx
+++ src/components/mainpage/News.tsx
@@ -109,170 +109,157 @@ export const News = () => {
   }, []);
 
   return (
-    <>
+    <Grid container direction="row" alignItems="center" justifyContent="center">
       <Grid
-        container
-        direction="row"
-        alignItems="center"
-        justifyContent="center"
+        item
+        sx={{
+          textAlign: "center",
+          background:
+            "linear-gradient(90deg, #5a9628 0%, #005A9C 50%, #5a9628 100%)",
+          color: "#FFFFFF",
+        }}
+        xs={12}
+        sm={8}
+        mt={6}
       >
-        <Grid
-          item
-          sx={{
-            textAlign: "center",
-            background:
-              "linear-gradient(90deg, #5a9628 0%, #005A9C 50%, #5a9628 100%)",
-            color: "#FFFFFF",
-          }}
-          xs={12}
-          sm={8}
-          mt={6}
-        >
-          <Typography variant="h1" mt={3} mb={3}>
-            Aktualności
-          </Typography>
-        </Grid>
-        {posts.length !== 0 ? (
-          <Grid item xs={12}>
-            <Grid
-              container
-              direction="row"
-              alignItems="stretch"
-              justifyContent="center"
-              sx={{ rowGap: { xs: 1, sm: 0 } }}
-            >
-              <>
-                <Grid item xs={12} md={3} sx={{ margin: "10px 0" }}>
-                  <Link
-                    href={`posts/${posts[0]?.id}`}
-                    style={{ textDecoration: "none" }}
-                  >
-                    <Paper sx={{ height: "100%", padding: "10px" }}>
-                      <Grid
-                        container
-                        direction="column"
-                        alignItems="flex-start"
-                        justifyContent="space-between"
-                        sx={{ height: "inherit" }}
-                      >
-                        <Grid item container direction="row">
-                          <Grid item xs={12}>
-                            <img
-                              src={posts[0].file && posts[0].file}
-                              alt="post image"
-                              style={{ width: "100%" }}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <Typography variant="h4" component="h3">
-                              {truncateString(posts[0]?.title, 55)}
-                            </Typography>
-                          </Grid>
-                        </Grid>
-                        <Grid item>
-                          {posts[0]?.date && (
-                            <Typography variant="caption">
-                              {dayjs(
-                                convertFirebaseTimestamp(posts[0]?.date)
-                              ).format("DD/MM/YYYY")}
-                            </Typography>
-                          )}
-                        </Grid>
-                      </Grid>
-                    </Paper>
-                  </Link>
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  md={5}
-                  sx={{ display: "flex", flexDirection: "column" }}
-                >
-                  <Grid
-                    container
-                    item
-                    direction="row"
-                    alignItems="flex-start"
-                    xs={12}
-                  >
-                    {posts.slice(1).map((post) => (
-                      <Grid
-                        item
-                        xs={6}
-                        sm={4}
-                        key={post.id}
-                        sx={{ padding: { xs: "0", sm: "10px" } }}
-                      >
-                        <Link
-                          href={`posts/${post.id}`}
-                          style={{ textDecoration: "none" }}
-                        >
-                          <Paper sx={{ height: "100%" }}>
-                            <Grid
-                              container
-                              direction="column"
-                              alignItems="flex-start"
-                              justifyContent="center"
-                              sx={{ padding: "10px" }}
-                            >
-                              <Grid
-                                item
-                                justifyContent={"center"}
-                                sx={{
-                                  position: "relative",
-                                  width: "100%",
-                                  textAlign: "center",
-                                  height: {
-                                    xs: "100px",
-                                    sm: "180px",
-                                    md: "120px",
-                                  },
-                                }}
-                              >
-                                <img
-                                  src={post.file && post.file}
-                                  alt="post image"
-                                  style={{
-                                    maxWidth: "100%",
-                                    height: "auto",
-                                    maxHeight: "100px",
-                                  }}
-                                />
-                              </Grid>
-                              <Grid item xs={12}>
-                                <Typography
-                                  variant="h6"
-                                  component="h4"
-                                  sx={{ minHeight: "64px" }}
-                                >
-                                  {truncateString(post.title, 40)}
-                                </Typography>
-                              </Grid>
-                              <Grid item xs={12}>
-                                {post?.date && (
-                                  <Typography variant="caption">
-                                    {dayjs(
-                                      convertFirebaseTimestamp(post?.date)
-                                    ).format("DD/MM/YYYY")}
-                                  </Typography>
-                                )}
-                              </Grid>
-                            </Grid>
-                          </Paper>
-                        </Link>
-                      </Grid>
-                    ))}
-                  </Grid>
-                </Grid>
-              </>
-            </Grid>
-          </Grid>
-        ) : (
-          <Grid item xs={12}>
-            <FootballLoader fixed={false} />
-          </Grid>
-        )}
+        <Typography variant="h1" mt={3} mb={3}>
+          Aktualności
+        </Typography>
       </Grid>
-    </>
+      {posts.length !== 0 ? (
+        <Grid item xs={12}>
+          <Grid
+            container
+            direction="row"
+            alignItems="stretch"
+            justifyContent="center"
+            sx={{ rowGap: { xs: 1, sm: 0 } }}
+          >
+            <>
+              <Grid item xs={12} md={3} sx={{ margin: "10px 0" }}>
+                <Link
+                  href={`posts/${posts[0]?.id}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Paper sx={{ height: "100%", padding: "10px" }}>
+                    <Grid
+                      container
+                      direction="column"
+                      alignItems="flex-start"
+                      justifyContent="space-between"
+                      sx={{ height: "inherit" }}
+                    >
+                      <Grid item container direction="row">
+                        <Grid item xs={12}>
+                          <img
+                            src={posts[0].file && posts[0].file}
+                            alt="post image"
+                            style={{ width: "100%" }}
+                          />
+                        </Grid>
+                        <Grid item xs={12} sx={{ padding: "10px 0" }}>
+                          <Typography variant="h4" component="h3">
+                            {posts[0]?.title}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                      <Grid item>
+                        {posts[0]?.date && (
+                          <Typography variant="caption">
+                            {dayjs(
+                              convertFirebaseTimestamp(posts[0]?.date)
+                            ).format("DD/MM/YYYY")}
+                          </Typography>
+                        )}
+                      </Grid>
+                    </Grid>
+                  </Paper>
+                </Link>
+              </Grid>
+              <Grid
+                container
+                item
+                direction="row"
+                alignItems="stretch"
+                xs={12}
+                md={5}
+              >
+                {posts.slice(1).map((post) => (
+                  <Grid
+                    item
+                    xs={6}
+                    sm={4}
+                    key={post.id}
+                    sx={{ padding: { xs: "0", sm: "10px" } }}
+                  >
+                    <Link
+                      href={`posts/${post.id}`}
+                      style={{ textDecoration: "none" }}
+                    >
+                      <Paper sx={{ height: "100%" }}>
+                        <Grid
+                          container
+                          direction="row"
+                          alignItems="flex-start"
+                          justifyContent="space-between"
+                          sx={{ padding: "10px", height: "inherit" }}
+                        >
+                          <Grid item xs={12} sx={{ height: "inherit" }}>
+                            <Grid
+                              item
+                              justifyContent={"center"}
+                              sx={{
+                                position: "relative",
+                                width: "100%",
+                                textAlign: "center",
+                              }}
+                            >
+                              <img
+                                src={post.file && post.file}
+                                alt="post image"
+                                style={{
+                                  maxWidth: "100%",
+                                  height: "auto",
+                                }}
+                              />
+                            </Grid>
+                            <Grid item xs={12}>
+                              <Typography
+                                variant="h4"
+                                component="h4"
+                                sx={{ minHeight: "64px", paddingTop: "10px" }}
+                              >
+                                {post.title}
+                              </Typography>
+                            </Grid>
+                          </Grid>
+                          <Grid
+                            item
+                            xs={12}
+                            sx={{ position: "relative", bottom: "15px" }}
+                          >
+                            {post?.date && (
+                              <Typography variant="caption">
+                                {dayjs(
+                                  convertFirebaseTimestamp(post?.date)
+                                ).format("DD/MM/YYYY")}
+                              </Typography>
+                            )}
+                          </Grid>
+                        </Grid>
+                      </Paper>
+                    </Link>
+                  </Grid>
+                ))}
+              </Grid>
+            </>
+          </Grid>
+        </Grid>
+      ) : (
+        <Grid item xs={12}>
+          <FootballLoader fixed={false} />
+        </Grid>
+      )}
+    </Grid>
   );
 };
